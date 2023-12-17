d47a58053a1d6c0022813a584a23b7a14426acd2
Abdul Rahman <abdulriver12344.com>
2023-12-17 10:17:44 +0700

    first commit

---
+++ Digital Clock Widget/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Clock_Widget
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
